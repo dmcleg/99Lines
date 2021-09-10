72a324b97e79a8d7429a4fe183f0b1ccb0fc7aac
dmcleg <dmcleghorn@gmail.com>
2021-09-10 12:37:59 -0500

    commit on main

--- 99Lines.js
+++ 99Lines.js
@@ -1,16 +1,14 @@
-var codeLeft = 99;
+let friends = ['Preston', 'Ben', 'John David', 'Hope', 'Sheffield']
 
-function NinetyNinelines (restofSong) {
-    console.log(codeLeft + ' lines of code in the file, ' + codeLeft + ' lines of code, Drew strikes one out, clears it all out, ' + codeLeft + ' lines of code in the file!')
-}
+for (let myFriends = 0; myFriends < friends.length; myFriends++) {
 
-while (codeLeft > 1) {
-
-    NinetyNinelines(codeLeft);
-
-    codeLeft--;
-
-	if (codeLeft == 1) {
-    console.log(codeLeft + ' line of code in the file, ' + codeLeft + ' line of code, Drew strikes one out, clears it all out, ' + codeLeft + ' line of code in the file!')
-	}
+    for (let codeLeft = 99; codeLeft > 0; codeLeft--) {
+        if (codeLeft > 2) {
+            console.log(codeLeft + ' lines of code in the file, ' + codeLeft + ' lines of code; ' + friends[myFriends] + ' strikes one out, clears it all out, ' + (codeLeft - 1) + ' lines of code in the file!')
+        } else if (codeLeft == 2) {
+            console.log(codeLeft + ' lines of code in the file, ' + codeLeft + ' lines of code; ' + friends[myFriends] + ' strikes one out, clears it all out, ' + (codeLeft - 1) + ' line of code in the file!')
+        } else {
+            console.log(codeLeft + ' line of code in the file, ' + codeLeft + ' line of code; ' + friends[myFriends] + ' strikes one out, clears it all out, ' + (codeLeft - 1) + ' lines of code in the file!')
+        }
+    }
 }
